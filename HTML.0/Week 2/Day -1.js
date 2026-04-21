e7cf19f485819709f06ccd11ef2e0e1cba2c7000
AyushGoyal527 <ayushgoyal527@gmail.com>
2026-04-21 21:38:37 +0530

    Delete HTML.0 directory

--- HTML.0/Week 2/Day -1.js
+++
@@ -1,8 +0,0 @@
-let str = "hello world";
-
-// let ans =  str.split(" ").reverse();
-// // console.log (ans
-
-let ans = str.split(" ").reverse();
-
-console.log(ans[0].split("").reverse().join("")).join(" ")
